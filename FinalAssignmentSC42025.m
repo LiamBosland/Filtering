d4acdeec02100ae6ffbccef8f5c637b0181b132f
SimonStouten <45373972+SimonStouten@users.noreply.github.com>
2018-12-22 11:58:31 +0100

    Created Hankel.m
    A function to compute the hankel matrix of a signal

---
+++ FinalAssignmentSC42025.m
@@ -0,0 +1,38 @@
+%% Final Assignment SC42025 Filtering and Identification
+% Written by Simon Stouten (4195981) and Liam Bosland(999999)
+%
+% The contents to this assignment are:
+%
+%
+%
+%
+% Practical Assignment 2018 - 2019
+% Turbulence Modeling for Adaptive Optics
+clc;
+clearvars;
+close all;
+
+%% Given data
+load systemMatrices.mat
+load turbulenceData.mat
+
+%% Model 1: Random-walk Model
+
+%% Model 2: Vector Auto-Regressive Model of Order 1
+% To formulate the VAR model of order 1 a number of steps have been taken
+% which are not discussed in this MATLAB code. For the full explanation and
+% elaboration to this model the reader is referred to the accompanying
+% report (pXX)
+
+% TEST: Take the first dataset of PhiIdent to test the model
+for i = 1 : 1 %length(phiIdent)
+    phi = phiIdent{1,i};
+    [p,N] = size(phi);
+    for j = 1 : 1 %length(phi)
+        % Since full acces to phi is available, it can be used to approximate
+        % the covariance matrices Cphi
+        Cphi0 = Cphi(phi(:,j));
+        Cphi1 = Cphi(phi(:,j),1);
+        [A,Cw,K] = computeKalmanAR(Cphi0,Cphi1,G,sigmae);
+    end
+end
