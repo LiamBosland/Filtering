7893e76790a27d99649bdd0cdc145fd43c69e17d
SimonStouten <45373972+SimonStouten@users.noreply.github.com>
2018-12-28 21:32:45 +0100

    Update computeKalmanAR.m

--- computeKalmanAR.m
+++ computeKalmanAR.m
@@ -21,7 +21,34 @@ end
 % Next is the computation of Cw:
 Cw = Cphi0 - (A*Cphi0*(A'));
 issymmetric(Cw)
-% Finally the Kalman gain-matrix can be computed using the DARE:
+
+% Before the Kalman-gain matrix can be computed, the two conditions to the
+% Kalman filter must be tested:
+
+% 1) The pair (A,G) must be observable.
+W = obsv(A,G);
+% W must be full-rank in order for the pair to be observable, this can be
+% checked using SVD again
+[~,Sw,~] = svd(W);
+Lw = length(nonzeros(round(diag(Sw),10))); % Find the number of nonzero elements of the singular values of W rounded up to 10 decimal points
+if Lw == size(W,2) % W is full column-rank
+else % W is not full column-rank
+   error('The pair (A,G^T) is not observable, provide different system matrices');  
+end
+
+% 2) The pair (A,sqrt(Q)) must be reachable. If its controllability
+% matrix full rank this implies reachability. For this case, Q = Cw
+Ctr = ctrb(A,sqrt(Cw));
+% Ctr must be full-rank in order for the pair to be controllabel thus 
+% reachable, this can be checked using SVD again
+[~,Sc,~] = svd(Ctr);
+Lc = length(nonzeros(round(diag(Sc),10))); % Find the number of nonzero elements of the singular values of Ctr rounded up to 10 decimal points
+if Lc == size(Ctr,1) % Ctr is full row-rank
+else % Ctr is not full row-rank
+   error('The pair (A,sqrt(Cw)) is not reachable, provide different system matrices');  
+end
+
+% If both conditions hold, the Kalman-gain matrix can be computed using the DARE:
 [~,~,Kt] = dare(A',G',Cw,sigmae*eye(size(G,2)));
 K = Kt';
 end
