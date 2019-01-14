d37f73e73ef2105fb1ae32de824873e812e1790e
SimonStouten <45373972+SimonStouten@users.noreply.github.com>
2019-01-14 18:21:17 +0100

    Updated files

--- FinalAssignmentSC42025.m
+++ FinalAssignmentSC42025.m
@@ -1,6 +1,5 @@
-
 %% Final Assignment SC42025 Filtering and Identification
-% Written by Simon Stouten (4195981) and Liam Bosland(999999)
+% Written by Simon Stouten (4195981) and Liam Bosland(4216377)
 %
 % The contents to this assignment are:
 %
@@ -54,15 +53,16 @@ ns = length(phiIdent);
 % For identification 2/3 of the data is used, the remaining 1/3 is used for
 % the validation process, using cross-validation.
 f_id = 2/3; f_val = 1-f_id;
-for i = ns
+for i = 1%ns
     % Simulate open-loop measurements so(k):
     phi = phiIdent{1,i};
     n = size(phi,1); N = size(phi,2); o = size(G,1); % Define some dimensions
-    r = 2*n+1
+    r = 2*n+1;
+    s_id = zeros(o,N);
     for k = 1 : N
         e = (sigmae^2*eye(o)*randn(o,1)); % Generate white noise sequence with covariance sigma^2*I
         s_id(:,k) = G*phi(:,k) + e;
-        [As,Cs,Ks] = SubId(s_id,f_id*N,f_val*N,r,n);
     end
+    [As,Cs,Ks] = SubId(s_id,ceil(f_id*N),floor(f_val*N),r,n);
 end
-    toc;
+toc;
--- Hankel.m
+++ Hankel.m
@@ -1,29 +1,38 @@
 function [Y] = Hankel(y,k,s,N)
-% Hankel(y,k,s,N) builds a Hankel matrix using vectorized input y, the 
-% starting index k the amount of block-rows s and the amount of 
-% block-columns N
+% Hankel(y,k,s,N) builds a Hankel matrix using vectorized input y, the
+% starting index k the amount of block-rows (of dimension ly) s and the
+% amount of block-columns N.
 
 % Default case, y is supplied and a square Hankel matrix is to be built
 if nargin == 1
     k = 1;
     s = floor(length(y)/2);
     N = ceil(length(y)/2);
-% The case where i is omitted, this the default is i = 1
+    % The case where k is omitted, the default is k = 1
 elseif nargin == 3
     st = k;
     N = s;
     s = st;
     k = 1;
-% The case where all information is supplied, this a custom Hankel matrix
-% is built
+    % The case where all information is supplied, this a custom Hankel matrix
+    % is built
 end
 
-% Checking if the inputs are of appropriate dimensions/criteria
-[Ny,ly] = size(y);
-if ly > 1 && Ny > 1
-    error('The input y is not a vector but a matrix. Please vectorize or supply a different input');
-elseif ly > 1 && Ny == 1
-    error('The input is a row vector, please supply a column vector');
+% Checking if the inputs are of appropriate dimensions/criteria.
+% Time-samples are required to be in the width of supplied y.
+[ly,Ny] = size(y);
+if ly == 1 || Ny == 1 % Supplied y is a vector
+    M = s-1;
+    if ly > Ny
+        y = y'; % Transpose y such that a column vector is obtained
+        [ly,Ny] = size(y);
+    end
+else % Supplied y is a matrix
+    M = 0;
+    if ly > Ny % Matrix y is deeper than wider (time-samples required in width)
+        y = y'; % Transpose y such that time-samples (assumed to be larger than measurement points y(k)) are in the width of y
+        [ly,Ny] = size(y);
+    end
 end
 % MATLAB cannot take zero indices, thus all k = 0 inputs are set to k = 1
 if s == 0
@@ -34,11 +43,12 @@ if N+s > Ny
 end
 
 c = 0;
-Y = zeros(s,N);
-    for i = k : s
-        for j = k : N
-            Y(i,j) = y(j+c);
-        end
-        c = c + 1;
+Y = zeros(s*ly,N);
+for i = 1 : s
+    a1 = (i-1)*(ly-1) + i; a2 = a1 + (ly-1); % Two counting parameters
+    for j = 1 : N
+        Y(a1:a2,j) = y(:,k+j+c-1);
     end
+    c = c + 1;
+end
 end
